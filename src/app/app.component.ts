c86edee84e7642a447e621c1d38b75f169fbca5c
Mollie <a-mmcintyre@vrbo.com>
2021-05-19 16:51:52 -0500

    Okta Oauth setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularProductCatalog';
+export class AppComponent implements OnInit {
+  public title = 'Angular Product Catalog';
+  public isAuthenticated: boolean;
+
+  constructor(public oktaAuth: OktaAuthService) {
+  	this.oktaAuth.$authenticationState.subscribe (
+      (isAuthenticated: boolean) => this.isAuthenticated = isAuthenticated
+    );
+  }
+
+  async ngOnInit() {
+    this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+   }
+
+  async login() {
+    await this.oktaAuth.signInWithRedirect();
+
+  }
+
+  async logout() {
+  	await this.oktaAuth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { OKTA_CONFIG, OktaAuthModule } from '@okta/okta-angular';
+import {
+  MatButtonModule,
+  MatDividerModule,
+  MatIconModule,
+  MatMenuModule,
+  MatProgressSpinnerModule,
+  MatTableModule,
+  MatToolbarModule
+} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+
+const oktaConfig = {
+  issuer: 'https://dev-3490370.okta.com/oauth2/default',
+  clientId: '0oarxth1io7zcSXKE5d6',
+  redirectUri: window.location.origin + '/callback'
+};
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    HomeComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatDividerModule,
+    MatProgressSpinnerModule,
+    FormsModule,
+    OktaAuthModule,
   ],
-  providers: [],
+  providers: [
+    { provide: OKTA_CONFIG, useValue: oktaConfig }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
